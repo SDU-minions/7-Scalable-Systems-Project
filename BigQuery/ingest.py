672f8578483db289459cc23a19516e5c8cbe3068
anton <antje12@student.sdu.dk>
2022-11-09 16:56:23 +0100

    feat: sample data import to kafka

--- BigQuery/ingest.py
+++ BigQuery/ingest.py
@@ -1,25 +1,104 @@
 # https://github.com/SohierDane/BigQuery_Helper
 from bq_helper import BigQueryHelper
 from kafka import KafkaProducer
+import json
 
 bq_assistant = BigQueryHelper("bigquery-public-data", "github_repos")
 k_producer = KafkaProducer(bootstrap_servers=['kafka-1:9092'])
 
-tables = bq_assistant.list_tables()
+def importRepos():
+    QUERY = """
+            select repo_name
+            from bigquery-public-data.github_repos.sample_repos
+            limit 50
+            """
+    res = bq_assistant.query_to_pandas_safe(QUERY)
 
-print("Tables found:")
-print(tables)
+    print(res.values.size, " repos found.")
+    for value in res.values:
+        repo_name = value[0]
+        k_producer.send('sample_repos', bytes(repo_name, 'utf-8'))
+        k_producer.flush()
 
-def tblPrint(table):
-    print("----------------------------------")
-    print(table + " table scheme") 
-    scheme = bq_assistant.table_schema(table)
-    print(scheme)
-    print("----------------------------------")
+def importCommits():
+    QUERY = """
+        select repo_name, commit, author, committer
+        from bigquery-public-data.github_repos.sample_commits
+        where repo_name in (
+        select repo_name
+        from bigquery-public-data.github_repos.sample_repos)
+        limit 50
+        """
+    res = bq_assistant.query_to_pandas_safe(QUERY)
 
-k_producer.send('test', bytes("It just works!", 'utf-8'))
+    print(res.values.size, " commits found.")
+    for val in res.values:
+        repo_name = val[0]
+        commit = val[1]
+        author = val[2]["name"]
+        date = val[2]["date"].isoformat()
+        data = {
+            'repo_name ': repo_name,
+            'commit' : commit,
+            'author' : author,
+            'date': date
+            }
+        k_producer.send('sample_commits', json.dumps(data).encode('utf-8'))
+        k_producer.flush()
 
-for table in tables:
-    # Write to a kafka topic with a KafkaProducer
-    k_producer.send('tables', bytes(table, 'utf-8'))
-    tblPrint(table)
+def importFiles():
+    QUERY = """
+            select repo_name, path
+            from bigquery-public-data.github_repos.sample_files
+            where repo_name in (
+            select repo_name
+            from bigquery-public-data.github_repos.sample_repos)
+            limit 50
+            """
+    res = bq_assistant.query_to_pandas_safe(QUERY, max_gb_scanned=6)
+
+    print(res.values.size, " files found.")
+    for val in res.values:
+        repo_name = val[0]
+        path = val[1]
+        data = {
+            'repo_name ': repo_name,
+            'path' : path
+            }
+        k_producer.send('sample_files', json.dumps(data).encode('utf-8'))
+        k_producer.flush()
+
+def importLanguages():
+    QUERY = """
+            select repo_name, language
+            from bigquery-public-data.github_repos.languages
+            where repo_name in (
+            select repo_name
+            from bigquery-public-data.github_repos.sample_repos)
+            limit 50
+            """
+    res = bq_assistant.query_to_pandas_safe(QUERY)
+
+    print(res.values.size, " languages found.")
+    for val in res.values:
+        repo_name = val[0]
+        language = val[1]
+        langauges = {}
+        for lang in language:
+            name = lang["name"]
+            bytes = lang["bytes"]
+            langauges[name] = bytes
+        data = {
+                'repo_name ': repo_name,
+                'langauges' : langauges
+                }
+        k_producer.send('languages', json.dumps(data).encode('utf-8'))
+        k_producer.flush()
+
+importRepos()
+importCommits()
+importFiles()
+importLanguages()
+
+k_producer.flush()
+k_producer.close()
