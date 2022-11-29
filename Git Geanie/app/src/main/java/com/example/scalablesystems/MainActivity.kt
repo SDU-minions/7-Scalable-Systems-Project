d9b7749f92e4881eab01079cf2bc39bd5cc70761
unknown <tes_7583@hotmail.com>
2022-11-29 23:45:21 +0100

    feat: dialogflow connect

--- Git Geanie/app/src/main/java/com/example/scalablesystems/MainActivity.kt
+++ Git Geanie/app/src/main/java/com/example/scalablesystems/MainActivity.kt
@@ -1,12 +1,90 @@
 package com.example.scalablesystems
 
-import androidx.appcompat.app.AppCompatActivity
+
+
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.api.gax.core.FixedCredentialsProvider
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.auth.oauth2.ServiceAccountCredentials
+import com.google.cloud.dialogflow.v2.DetectIntentRequest
+import com.google.cloud.dialogflow.v2.QueryInput
+import com.google.cloud.dialogflow.v2.SessionName
+import com.google.cloud.dialogflow.v2.SessionsClient
+import com.google.cloud.dialogflow.v2.SessionsSettings
+import com.google.cloud.dialogflow.v2.TextInput
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.UUID
 
 class MainActivity : AppCompatActivity() {
+    //https://github.com/veeyaarVR/dialogflow_android_kotlin
+
+    //dialogFlow
+    private var sessionsClient: SessionsClient? = null
+    private var sessionName: SessionName? = null
+    private val uuid = UUID.randomUUID().toString()
+    private val TAG = "mainactivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        //initialize bot config
+        setUpBot()
+
+        //test dialogflow
+        sendMessageToBot("top 10")
     }
+
+    private fun setUpBot() {
+        try {
+            val stream = this.resources.openRawResource(R.raw.credentials)
+            val credentials: GoogleCredentials = GoogleCredentials.fromStream(stream)
+                .createScoped("https://www.googleapis.com/auth/cloud-platform")
+            //val projectId: String = (credentials as ServiceAccountCredentials).projectId
+            val projectId: String = "myfirstagent-htrt"
+            val settingsBuilder: SessionsSettings.Builder = SessionsSettings.newBuilder()
+            val sessionsSettings: SessionsSettings = settingsBuilder.setCredentialsProvider(
+                FixedCredentialsProvider.create(credentials)
+            ).build()
+            sessionsClient = SessionsClient.create(sessionsSettings)
+            sessionName = SessionName.of(projectId, uuid)
+            Log.d(TAG, "projectId : $projectId")
+        } catch (e: Exception) {
+            Log.d(TAG, "setUpBot: " + e.message)
+        }
+    }
+
+    private fun sendMessageToBot(message: String) {
+        val input = QueryInput.newBuilder()
+            .setText(TextInput.newBuilder().setText(message).setLanguageCode("en-US")).build()
+        GlobalScope.launch {
+            sendMessageInBg(input)
+        }
+    }
+
+    private suspend fun sendMessageInBg(
+        queryInput: QueryInput
+    ) {
+        withContext(Default) {
+            try {
+                val detectIntentRequest = DetectIntentRequest.newBuilder()
+                    .setSession(sessionName.toString())
+                    .setQueryInput(queryInput)
+                    .build()
+                val result = sessionsClient?.detectIntent(detectIntentRequest)
+                Log.i(TAG, result.toString())
+            } catch (e: java.lang.Exception) {
+                Log.d(TAG, "doInBackground: " + e.message)
+                e.printStackTrace()
+            }
+        }
+    }
+
+
 }
