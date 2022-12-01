a4778dec0ae15f2ef3bc4c74d3fbad0942839df7
unknown <tes_7583@hotmail.com>
2022-12-01 21:41:40 +0100

    feat: speech to text working

--- Git Geanie/app/src/main/java/com/example/scalablesystems/MainActivity.kt
+++ Git Geanie/app/src/main/java/com/example/scalablesystems/MainActivity.kt
@@ -2,23 +2,24 @@ package com.example.scalablesystems
 
 
 
+import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.api.gax.core.FixedCredentialsProvider
 import com.google.auth.oauth2.GoogleCredentials
-import com.google.auth.oauth2.ServiceAccountCredentials
-import com.google.cloud.dialogflow.v2.DetectIntentRequest
-import com.google.cloud.dialogflow.v2.QueryInput
-import com.google.cloud.dialogflow.v2.SessionName
-import com.google.cloud.dialogflow.v2.SessionsClient
-import com.google.cloud.dialogflow.v2.SessionsSettings
-import com.google.cloud.dialogflow.v2.TextInput
+import com.google.cloud.dialogflow.v2.*
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.util.UUID
+import java.util.*
+import android.content.Intent
+import android.net.Uri
 
 class MainActivity : AppCompatActivity() {
     //https://github.com/veeyaarVR/dialogflow_android_kotlin
@@ -29,6 +30,14 @@ class MainActivity : AppCompatActivity() {
     private val uuid = UUID.randomUUID().toString()
     private val TAG = "mainactivity"
 
+    // on below line we are creating variables
+    // for text view and image view
+    lateinit var outputTV: TextView
+    lateinit var micIV: ImageView
+
+    // on below line we are creating a constant value
+    private val REQUEST_CODE_SPEECH_INPUT = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -36,6 +45,51 @@ class MainActivity : AppCompatActivity() {
 
         //initialize bot config
         setUpBot()
+
+        //initialize mic config
+        // initializing variables of list view with their ids.
+        outputTV = findViewById(R.id.idTVOutput)
+        micIV = findViewById(R.id.idIVMic)
+
+        // on below line we are adding on click
+        // listener for mic image view.
+        micIV.setOnClickListener {
+            // on below line we are calling speech recognizer intent.
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+            // on below line we are passing language model
+            // and model free form in our intent
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+
+            // on below line we are passing our
+            // language as a default language.
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault()
+            )
+
+            // on below line we are specifying a prompt
+            // message as speak to text on below line.
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text")
+
+            // on below line we are specifying a try catch block.
+            // in this block we are calling a start activity
+            // for result method and passing our result code.
+            try {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+            }  catch (e: Exception) {
+                // on below line we are displaying error message in toast
+                Toast
+                    .makeText(
+                        this@MainActivity, " " + e.message,
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+            }
+        }
 
         //test dialogflow
         sendMessageToBot("top 10")
@@ -111,6 +165,85 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+
+
+    private fun setUpMic(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // initializing variables of list view with their ids.
+        outputTV = findViewById(R.id.idTVOutput)
+        micIV = findViewById(R.id.idIVMic)
+
+        // on below line we are adding on click
+        // listener for mic image view.
+        micIV.setOnClickListener {
+            // on below line we are calling speech recognizer intent.
+            val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+
+            // on below line we are passing language model
+            // and model free form in our intent
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+
+            // on below line we are passing our
+            // language as a default language.
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault()
+            )
+
+            // on below line we are specifying a prompt
+            // message as speak to text on below line.
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text")
+
+            // on below line we are specifying a try catch block.
+            // in this block we are calling a start activity
+            // for result method and passing our result code.
+            try {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT)
+            } catch (e: Exception) {
+                // on below line we are displaying error message in toast
+                Toast
+                    .makeText(
+                        this@MainActivity, " " + e.message,
+                        Toast.LENGTH_SHORT
+                    )
+                    .show()
+            }
+        }
+    }
+
+
+
+    // on below line we are calling on activity result method.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // in this method we are checking request
+        // code with our result code.
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            // on below line we are checking if result code is ok
+            if (resultCode == RESULT_OK && data != null) {
+
+                // in that case we are extracting the
+                // data from our array list
+                val res: ArrayList<String> =
+                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
+
+                // on below line we are setting data
+                // to our output text view.
+                outputTV.setText(
+                    Objects.requireNonNull(res)[0]
+                )
+            }
+        }
+    }
+
+
 
 
 }
